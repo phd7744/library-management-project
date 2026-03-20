8b1476963138fb3eea67892b010f76d6699e5a74
PhDxunq <you@example.com>
2026-03-20 11:55:14 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.project_oop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.example.project_oop to javafx.fxml;
+    exports com.example.project_oop;
+    exports com.example.project_oop.controller;
+    opens com.example.project_oop.controller to javafx.fxml;
+}
